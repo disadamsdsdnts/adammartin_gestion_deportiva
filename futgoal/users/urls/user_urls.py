a9294c472d16216c1049598d39b0aab71989bbbf
Adán Martin <adanmartin@baetica.com>
2025-06-07 11:59:46 +0200

    DEV: Eliminar código antiguo

--- futgoal/users/urls/user_urls.py
+++ futgoal/users/urls/user_urls.py
@@ -6,35 +6,4 @@ from django.urls import include, path, re_path
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-
-    # path(
-    #     '',
-    #     GaUserListView.as_view(),
-    #     name='ga_user_list'
-    # ),
-    # path(
-    #     'new-user/',
-    #     GaUserCreateView.as_view(),
-    #     name='ga_user_create'
-    # ),
-    # path(
-    #     'detail/<pk>/',
-    #     GaUserDetailView.as_view(),
-    #     name='ga_user_detail'
-    # ),
-    # path(
-    #     'update/<pk>/',
-    #     GaUserUpdateView.as_view(),
-    #     name='ga_user_update'
-    # ),
-    # path(
-    #     'delete/<pk>/',
-    #     GaUserDeleteView.as_view(),
-    #     name='ga_user_delete'
-    # ),
-    # path(
-    #     'create/',
-    #     GaUserCreateView.as_view(),
-    #     name='ga_user_create'
-    # ),
 ]
